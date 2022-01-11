d6f2026e680026df56db36d726ef53061290bfa3
duboo <duboo31@gmail.com>
2022-01-12 07:47:32 +0900

    Done header and navbar

---
+++ plasta/main.js
@@ -0,0 +1,46 @@
+const checkScroll = () => {
+  let pageYOffSet = window.pageYOffset;
+  const upperContainer = document.getElementById('upper-container');
+
+  if(pageYOffSet > 200) {
+    upperContainer.style.backgroundColor = "rgba(0,0,0,0.4)"
+  } else {
+    upperContainer.style.backgroundColor = ""
+  }
+}
+
+const hambugerMenu = document.getElementById('hambuger-icon');
+
+const showNavSection = () => {
+  const navContainer = document.getElementById('nav-container');
+  const windowWidth = window.innerWidth;  
+  
+  if(windowWidth > 1023) {
+    navContainer.style.width = "340px"
+    navContainer.style.right = "0"
+  } else {
+    navContainer.style.width = "100%"
+    navContainer.style.right = "0"
+  }
+}
+
+const closeBt = document.getElementById('closeBt');
+
+const closeNavBar = () => {
+  const navContainer = document.getElementById('nav-container');
+  const windowWidth = window.innerWidth;  
+
+  // Desktop
+  if(windowWidth > 1023) {
+    navContainer.style.width = "340px"
+    navContainer.style.right = "-340px";
+  } else {
+    //Mobile
+    navContainer.style.width = "100%"
+    navContainer.style.right = "-100%";
+  }
+}
+
+closeBt.addEventListener('click', closeNavBar);
+hambugerMenu.addEventListener('click', showNavSection);
+window.addEventListener('scroll', checkScroll);
